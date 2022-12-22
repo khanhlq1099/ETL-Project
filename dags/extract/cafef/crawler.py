a9741130aa340cac17273f43c261a81e9d31535b
Gumball Watterson <mac@Gumball-Watterson.local>
2022-12-22 16:01:47 +0700

    update crawler.py p2

--- dags/extract/cafef/crawler.py
+++ dags/extract/cafef/crawler.py
@@ -8,7 +8,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 
 from bs4 import BeautifulSoup
